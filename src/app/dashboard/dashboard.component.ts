6d646f694fde6d3b197049c80609409f59edbf54
Luis181996 <luishumberto181996@hotmail.com>
2024-06-22 18:56:13 -0500

    parte 7

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../angular-material/material/material.module';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  constructor(private router: Router, 
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ){
+
+  }
+
+  salir(): void{
+    this.authService.logout();
+    this.router.navigateByUrl("/login")
+  }
+
+
+  irHome():void{
+    this.router.navigate(["home"], {relativeTo: this.route})
+  }
+  irCurso():void{
+    this.router.navigate(["curso"], {relativeTo: this.route})
+  }
+  irPost():void{
+    this.router.navigate(["post"], {relativeTo: this.route})
+  }
+  irEstado():void{
+    this.router.navigate(["estado"], {relativeTo: this.route})
+  }
 
 }
